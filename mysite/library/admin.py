102e6ff0b7bf0e31e5fc3ad548b7a5b6e08ad1f3
DonatasNoreika <donoras@gmail.com>
2024-10-09 09:51:53 +0300

    + 2: sukūrėme custom Knygų kopijų puslapį

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -4,9 +4,17 @@ from .models import Book, BookInstance, Author, Genre
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'isbn', 'author', 'display_genre']
 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'book', 'due_back', 'status']
+    list_filter = ['book', 'status', 'due_back']
+
+    fieldsets = (
+        ("Pagrindinis", {"fields": ("uuid", "book")}),
+        ("Pasiekiamumas", {"fields": ("due_back", "status")}),
+    )
 
 # Register your models here.
 admin.site.register(Book, BookAdmin)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
 admin.site.register(Author)
 admin.site.register(Genre)
