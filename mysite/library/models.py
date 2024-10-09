57d48eb945f6dcf2fc70f1048f104119eac9d0e6
DonatasNoreika <donoras@gmail.com>
2024-10-09 09:23:22 +0300

    + 2: pakeisti modelių pavadinimus vienaskaita/daugiskaita

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 
+
 # Create your models here.
 class Author(models.Model):
     first_name = models.CharField(verbose_name="Vardas", max_length=100)
@@ -9,22 +10,39 @@ class Author(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+    class Meta:
+        verbose_name = "Autorius"
+        verbose_name_plural = "Autoriai"
+
+
 class Genre(models.Model):
-    name = models.CharField(verbose_name="Pavadinimas", max_length=200, help_text='Įveskite knygos žanrą (pvz. detektyvas)')
+    name = models.CharField(verbose_name="Pavadinimas", max_length=200,
+                            help_text='Įveskite knygos žanrą (pvz. detektyvas)')
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "Žanras"
+        verbose_name_plural = "Žanrai"
 
 
 class Book(models.Model):
     title = models.CharField(verbose_name="Pavadinimas", max_length=200)
     summary = models.TextField(verbose_name="Aprašymas", max_length=2000, help_text='Trumpas knygos aprašymas')
-    isbn = models.CharField(verbose_name="ISBN", max_length=13, help_text='13 Simbolių <a href="https://www.isbn-international.org/content/what-isbn">ISBN kodas</a>')
-    author = models.ForeignKey(to="Author", verbose_name="Autorius", on_delete=models.SET_NULL, null=True, blank=True, related_name="books")
+    isbn = models.CharField(verbose_name="ISBN", max_length=13,
+                            help_text='13 Simbolių <a href="https://www.isbn-international.org/content/what-isbn">ISBN kodas</a>')
+    author = models.ForeignKey(to="Author", verbose_name="Autorius", on_delete=models.SET_NULL, null=True, blank=True,
+                               related_name="books")
     genre = models.ManyToManyField(to="Genre", verbose_name="Žanrai", help_text='Išrinkite žanrą (-us) šiai knygai')
 
     def __str__(self):
         return f"{self.title} ({self.author})"
+
+    class Meta:
+        verbose_name = "Knyga"
+        verbose_name_plural = "Knygos"
+
 
 class BookInstance(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, help_text='Unikalus ID knygos kopijai')
@@ -40,3 +58,7 @@ class BookInstance(models.Model):
 
     def __str__(self):
         return f"{self.uuid} - {self.book.title} ({self.get_status_display()}, {self.due_back})"
+
+    class Meta:
+        verbose_name = "Kopija"
+        verbose_name_plural = "Kopijos"
