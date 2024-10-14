20bbfb011a87cd361398620d7b7005a97a3bceb1
DonatasNoreika <donoras@gmail.com>
2024-10-14 10:45:11 +0300

    + 5: Knygų sąraše rodomi jų viršeliai. Jei viršelio nėra, rodoma no-image iliustracija.

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -38,7 +38,7 @@ class BookListView(generic.ListView):
     model = Book
     template_name = "books.html"
     context_object_name = "books"
-    paginate_by = 4
+    paginate_by = 6
 
 
 class BookDetailView(generic.DetailView):
