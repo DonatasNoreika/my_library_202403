c5b115844fbec0b2b571ac2f3e6eb3ee257f0c61
DonatasNoreika <donoras@gmail.com>
2024-10-14 10:14:58 +0300

    + 5: Sukurta paieška tarp knygų ir autorių

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book, BookInstance, Author
 from django.views import generic
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 
@@ -44,3 +45,19 @@ class BookDetailView(generic.DetailView):
     model = Book
     template_name = "book.html"
     context_object_name = "book"
+
+def search(request):
+    query = request.GET.get("query")
+    book_search_results = Book.objects.filter(Q(title__icontains=query) | Q(summary__icontains=query) |
+                                              Q(isbn__icontains=query) | Q(author__first_name__icontains=query) |
+                                              Q(author__last_name__icontains=query))
+
+    author_search_results = Author.objects.filter(
+        Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(description__icontains=query))
+
+    context = {
+        "query": query,
+        "books": book_search_results,
+        "authors": author_search_results,
+    }
+    return render(request, template_name="search.html", context=context)
