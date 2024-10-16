14267569151baceb2092d9cdd06eb43a3a4d219e
DonatasNoreika <donoras@gmail.com>
2024-10-16 09:25:55 +0300

    + 7: Jei kopiją vėluojama gražinti, ji nuspalvinama raudonai

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -141,5 +141,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'library/media')
 MEDIA_URL = '/media/'
 # print(MEDIA_ROOT) - nevenkite padebuginti, bus lengviau nepasiklysti django filesystem džiunglėse
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/library/'
 LOGOUT_REDIRECT_URL = '/'
