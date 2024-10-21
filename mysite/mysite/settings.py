0b6994db99ae1d0a61f45a73fa6c0948eae5a343
DonatasNoreika <donoras@gmail.com>
2024-10-21 09:27:59 +0300

    + 9: Profilio atnaujinimo forma apipavidalinta su crispy biblioteka

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -43,6 +43,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'library',
     'tinymce',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -173,3 +175,6 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
     }
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
